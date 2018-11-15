92e9303efab8ff2071829596e861071f7f5b0878
kippo <kippbrady@gmail.com>
2018-11-16 09:35:28 +1030

    Adding axios and pulling mock datat into event list

--- app/frontend/javascripts/react/event_browse.jsx
+++ app/frontend/javascripts/react/event_browse.jsx
@@ -1,3 +1,4 @@
+import Axios from 'axios';
 import React from "react";
 import EventList from "./event_list.jsx";
 import EventFilters from "./event_filters.jsx";
@@ -5,31 +6,46 @@ import EventFilters from "./event_filters.jsx";
 export default class EventBrowse extends React.Component {
   constructor(props){
     super(props);
-    this.eventData = [
-      {
+    this.state = {
+      eventData: [{
         title: 'Fuego Carnal',
         genre: 'Comedy',
         venue: 'Garden of Unearthly Delights',
-        date: '21 July to 26th July',
-        description: 'Dave is single, stood with his mates at the bar, and is turning 30 next week. Eurydice is a mythical dryad - a tree nymph. This is a tale of impossible, death defying love; a tale of this world and the underworld; a tale of soul music and late night karaoke.',
-        audienceReview: 3
+        date: '21 July to 26th July'
       },{
         title: 'Wet Sounds',
         genre: 'Drama',
         venue: 'Gluttony',
-        date: '25 July to 28th July',
-        description: 'A spectacular new Spiegeltent corruption from the people who brought you BLANC de BLANC and LIMBO UNHINGED. A joyous cocktail of entertainment and enlightenment, let this cast of international prodigies and deviants lead you on an adventure of hedonistic theatrical anarchy.',
-        audienceReview: 3
-      }
-    ];
+        date: '25 July to 28th July'
+      }]
+    }
   }
+
+  getEvents = () => {
+    Axios
+      .get('https://my.api.mockaroo.com/fringe.json?key=482c6d90')
+      .then((response) => {
+        const eventData = response.data;
+        return eventData;
+      })
+  }
+
+  /*
+  componentWillMount() {
+    this.setState(
+      {eventData: this.getEvents()}
+    );
+  }
+  */
 
   render() {
     return(
       <div>
         <EventFilters />
-        <EventList eventData={this.eventData} />
+        <EventList eventData={this.state.eventData} />
       </div>
     )
   }
 };
+
+// Pass data into react component
--- app/frontend/javascripts/react/event_filters.jsx
+++ app/frontend/javascripts/react/event_filters.jsx
@@ -3,15 +3,16 @@ import React from "react";
 export default class EventFilters extends React.Component {
   constructor(props){
     super(props);
+    this.genres = ["All", "Comedy", "Drama", "Circus", "Cabaret"]
   }
 
   render() {
     return(
       <select>
-        <option value="all">All</option>
-        <option value="comedy">Comedy</option>
-        <option value="drama">Drama</option>
-        <option value="circus">Circus</option>
+        {this.genres.map(function(genre) {
+            return <option>{genre}</option>
+          }
+        )}
       </select>
     )
   }
