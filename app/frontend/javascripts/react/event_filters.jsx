0622ac156b234af797c1cf1a2d75461e80fbb396
kippo <kippbrady@gmail.com>
2018-12-07 14:07:19 +1030

    Refactor of filters

--- app/frontend/javascripts/react/event_filters.jsx
+++ app/frontend/javascripts/react/event_filters.jsx
@@ -3,71 +3,68 @@ import React from "react";
 export default class EventFilters extends React.Component {
   constructor(props){
     super(props);
-    this.genres = ["All", "Comedy", "Drama", "Circus", "Cabaret"];
-    this.venues = ["All", "Garden of Unearthly Delights", "Gluttony", "Black Cat"];
+    this.genres = ["Comedy", "Drama", "Circus", "Cabaret"];
+    this.venues = ["Garden of Unearthly Delights", "Gluttony", "Black Cat"];
     this.selectedFilters = {};
   }
 
-  filterChange = (e, filterStructure) => {
-    let filterType = e.target.name;
-    let filterValue = e.target.value;
-
-    if (filterValue !== "All") {
-      if (filterStructure === "array") {
-        this.manageFilterArrays(filterType, filterValue);
-      } else if (filterStructure === "string") {
-        this.manageFilterStrings(filterType, filterValue);
-      }
-    } else {
-      delete this.selectedFilters[filterType];
+  filterChange = (e, formType, filterType) => {
+    if (formType === "checkbox") {
+      this.filterArrays(filterType, e.target.value);
+    } else if (formType === "select") {
+      this.filterString(e.target.name, e.target.value);
     }
+
     this.props.filterCallback(this.selectedFilters);
   }
 
   //Add or remove filter options stored as arrays
-  manageFilterArrays = (filterType, filterValue) => {
-    //If array exists modify existing array
-    if (this.selectedFilters[filterType] !== undefined) {
-      //If not in array add it
-      if (!this.selectedFilters[filterType].includes(filterValue)) {
-        this.selectedFilters[filterType].push(filterValue);
-      } else {
-      //Else remove it
-        if (true) {
-          let filteredArray = this.selectedFilters[filterType].filter((item) => {
-            return item !== filterValue;
-          });
-          this.selectedFilters[filterType] = filteredArray;
-        } else {
-          console.log('All filters');
-        }
-      }
-    //Else create new array
-    } else {
-      this.selectedFilters[filterType] = [filterValue];
+  filterArrays = (filterKey, filterValue) => {
+    let filterArray = [];
+    
+    // If array already exists use it instead
+    if (Array.isArray(this.selectedFilters[filterKey])) {
+      filterArray = this.selectedFilters[filterKey];
     }
+
+    // Check to see if filter value is already in array, if not add it
+    if (!filterArray.includes(filterValue)) {
+      filterArray.push(filterValue);
+    } else {
+    // Else remove it
+      let filterValueIndex = filterArray.indexOf(filterValue);
+      if (filterValueIndex > -1) {
+        filterArray.splice(filterValueIndex, 1);
+      }
+    }
+
+    this.selectedFilters[filterKey] = filterArray;
   }
   
   //Add or remove filter options stored as strings
-  manageFilterStrings = (filterType, filterValue) => {
-    if (this.selectedFilters[filterType] !== filterValue) {
-      this.selectedFilters[filterType] = filterValue;
+  filterString = (filterKey, filterValue) => {
+    if (this.selectedFilters[filterKey] !== filterValue) {
+      this.selectedFilters[filterKey] = filterValue;
     } else {
-      delete this.selectedFilters[filterType];
+      delete this.selectedFilters[filterKey];
     }
   }
 
   render() {
     return(
       <div>
-        <select onChange={(e) => this.filterChange(e, 'array')} name="genre">
-          {this.genres.map(function(genre) {
-              return <option key={genre}>{genre}</option>
+        <div>
+          {this.genres.map(genre => {
+              return (
+                <label key={genre}>
+                  <input type="checkbox" name="genre" value={genre} onChange={e => this.filterChange(e, 'checkbox', 'genre')} /> {genre}
+                </label>
+              )
             }
           )}
-        </select>
-        <select onChange={(e) => this.filterChange(e, 'string')} name ="venue">
-          {this.venues.map(function(venue) {
+        </div>
+        <select onChange={e => this.filterChange(e, 'select', 'venue')} name="venue">
+          {this.venues.map(venue => {
               return <option key={venue}>{venue}</option>
             }
           )}
@@ -76,11 +73,3 @@ export default class EventFilters extends React.Component {
     )
   }
 };
-
-//Remove key from array function if all options selected manually
-/* 
-  Remove propoerty when
-  'All' option selected
-  All options selected
-  All options deselected
-*/
